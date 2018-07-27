9ab60c0d2be6f2aa6c04cf1e116d68a8eeb9d5d1
kaushnian <kefbox@gmail.com>
2018-07-26 23:52:26 -0700

    Add List Item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListitemComponent } from './listitem/listitem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListitemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/listitem/listitem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'todo-listitem',
+  templateUrl: './listitem.component.html',
+  styleUrls: ['./listitem.component.css']
+})
+export class ListitemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
